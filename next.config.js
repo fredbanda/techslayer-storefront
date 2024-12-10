1614356ae929863b6c103055ff51d74e5351c39e
Fred Banda <fredcbanda@hotmail.com>
2024-12-10 13:26:47 +0200

    win

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true, // This is the correct place to set the unoptimized option for images
+//   },
+//   output: "export", // Ensures the app will be statically exported
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  unoptimized: true,
-  output: "export"
-}
+  images: {
+    unoptimized: true, // This is the correct place to set the unoptimized option for images
+  },// Ensures the app will be statically exported
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
